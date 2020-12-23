e2cf07c4a16d0305fac6e12ae878ee9403d00729
Emily Bender <bender.emily1@gmail.com>
2020-12-23 16:06:18 -0500

    Created Health Component and Added to Pawns

--- Source/ToonTanks/GameModes/ToonTankGameModeBase.cpp
+++ Source/ToonTanks/GameModes/ToonTankGameModeBase.cpp
@@ -15,6 +15,7 @@ void AToonTankGameModeBase::BeginPlay()
 void AToonTankGameModeBase::ActorDied(AActor* DeadActor)
 {
 	// Check what type of Actor died. If Turret, tally. If Player -> go to lose condition. 
+	UE_LOG(LogTemp, Warning, TEXT("A Pawn Died"));
 }
 
 void AToonTankGameModeBase::HandleGameStart()
